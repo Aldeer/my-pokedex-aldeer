2e6c0b0e73152b0f3850382779b71832b06fe632
Emanuel Gomila <gomilaemanuel@gmail.com>
2022-06-27 19:17:56 -0300

    creacion del componente tarjeta donde se mostrara la info del pokemon

---
+++ src/components/CardPokemon.js
@@ -0,0 +1,15 @@
+//react
+import { Fragment } from "react";
+
+//react-router
+import {Link} from "react-router-dom";
+
+const CardPokemon = (props) => {
+  const {pokemon} = props;
+  console.log(pokemon)
+  return (
+    <div className="col"></div>
+  );
+}
+
+export default CardPokemon
