c20c14d0b2611fe370c663758ae70152bc8c143e
Emanuel Gomila <gomilaemanuel@gmail.com>
2022-06-27 19:36:27 -0300

    tarjeta de pokemon creada, con titulo y sprite del pokemon

--- src/components/CardPokemon.js
+++ src/components/CardPokemon.js
@@ -5,10 +5,20 @@ import { Fragment } from "react";
 import {Link} from "react-router-dom";
 
 const CardPokemon = (props) => {
+  //props
   const {pokemon} = props;
-  console.log(pokemon)
+
   return (
-    <div className="col"></div>
+    <Fragment>
+      <div className="col-4">
+        <div className="card shadow rounded m-3">
+          <div className="card-body">
+            <img src={pokemon.sprites.front_default} alt="..." className="card-img-top" />
+            <h5 className="card-title text-center">{pokemon.name}</h5>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
